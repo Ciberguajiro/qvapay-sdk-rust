78f48813ad337716a32b3888dd5acd16126243c6
Ciberguajiro <bryanlenier@gmail.com>
2026-04-27 11:53:57 -0400

    fix model auth

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -32,6 +32,7 @@ pub struct AuthLoginResponse2FA {
 /// Successful login response.
 #[derive(Deserialize, Debug, Clone)]
 pub struct AuthLoginResponse {
+    #[serde(rename = "accessToken")]
     pub access_token: String,
     pub token_type: String,
     pub me: Profile,
@@ -41,10 +42,37 @@ pub struct AuthLoginResponse {
 #[derive(Deserialize, Debug, Clone)]
 pub struct Profile {
     pub uuid: String,
-    pub email: String,
-    pub name: String,
     pub username: String,
+    pub name: String,
+    pub lastname: String,
+    pub email: String,
+    pub two_factor_reset_code: String,
+    pub bio: String,
+    pub address: String,
+    pub image: String,
+    pub cover: Option<String>,
     pub balance: String,
+    pub pending_balance: String,
+    pub satoshis: String,
+    #[serde(rename = "createdAt")]
+    pub created_at: String,
+    #[serde(rename = "updatedAt")]
+    pub updated_at: String,
+    pub phone: String,
+    pub phone_verified: String,
+    pub telegram: String,
+    pub twitter: Option<String>,
+    pub kyc: bool,
+    pub kyc_status: String,
+    pub vip: bool,
+    pub golden_check: bool,
+    pub referral_source: Option<String>,
+    pub registration_platform: Option<String>,
+    pub pin: i32,
+    pub last_seen: String,
+    pub telegram_id: String,
+    pub role: String,
+    pub p2p_enabled: bool,
 }
 
 /// Request for registration operation.
